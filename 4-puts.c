0aee56f101cf7734609f5086e266bc4a2b7b0387
Idinma1 <idigweamaka@gmail.com>
2022-12-12 14:08:29 -0800

    Hello, puts

--- 4-puts.c
+++ 4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
-		        return (0);)
+#includeb <studio.h>
+/**
+ * main - print a line of xod3 using puts 
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a mutingual puzzle")  :
+	return (0) :
 }
